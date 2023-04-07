89c249735636252b423acd5407ed98e1b8936de5
heeeedjr <heeeedjr@gmail.com>
2023-04-07 11:24:33 -0600

    create database connection

---
+++ JS/database.js
@@ -0,0 +1,22 @@
+const {MongoClient} = require('mongodb');
+
+const userName = process.env.MONGOUSER;
+const password = process.env.MONGOPASSWORD;
+const hostname = process.env.MONGOHOSTNAME;
+
+if (!userName) {
+  throw Error('Database not configured. Set environment variables');
+}
+
+const url = `mongodb+srv://${userName}:${password}@${hostname}`;
+
+const client = new MongoClient(url);
+const blogCollection = client.db('cs260').collection('blogPosts');
+
+// Add blog post
+
+// Get blog post
+
+// Delete blog post
+
+// Update blog post
