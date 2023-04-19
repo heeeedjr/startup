cff49b122bab575d2a67c9d23f740a07ef0e95b6
heeeedjr <heeeedjr@gmail.com>
2023-04-18 23:52:48 -0600

    file location change again

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const cookieParser = require('cookie-parser');
 const bcrypt = require('bcrypt');
 const express = require('express');
 const app = express();
-const DB = require('./public/JS/database.js');
-const { peerProxy } = require('./public/JS/peerProxy.js');
+const DB = require('./public/database.js');
+const { peerProxy } = require('./public/peerProxy.js');
 
 const authCookieName = 'token';
 
 // The service port. In production the application is statically hosted by the service on the same port.
-const port = process.argv.length > 2 ? process.argv[2] : 4000;
+const port = process.argv.length > 2 ? process.argv[2] : 3000;
 
 // JSON body parsing using built-in middleware
 app.use(express.json());
